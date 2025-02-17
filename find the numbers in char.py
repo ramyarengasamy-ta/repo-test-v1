1a4284e8ff5648b84ba670da3d2f629ebf992eaf
ramyarengasamy-ta <ramya.rengasamy@tigeranalytics.com>
2025-02-17 18:37:03 +0530

    char

---
+++ find the numbers in char.py
@@ -0,0 +1,9 @@
+
+str = input("enter a str")
+ 
+count = 0  
+for char in str:
+    count += 1
+ 
+print(str)  
+print(count)
---
+++ range.py
@@ -0,0 +1,10 @@
+int=range(10)
+for num in int:
+    print(num)
+int2=range(4) 
+for num in int2:
+    print(num)   
+    int3=range(7)
+    for num in int3:
+        print(num)
+        
