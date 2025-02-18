52c494268ce1325ec80421ed3c5555c671e83bda
ramyarengasamy-ta <ramya.rengasamy@tigeranalytics.com>
2025-02-18 11:18:14 +0530

    list samples

---
+++ index.py
@@ -0,0 +1,10 @@
+num = [[0, 2], [4, 6], [8, 10], [12, 14]]
+print(num[0])
+print(num[3][1])
+furniture = ["chair", "table", "desk", "lamp", "bed"]
+print(furniture[-5])
+
+floats = [0.98, 8.76, 6.54, 4.32]
+print(floats[1:])
+print(floats[1:3])
+print(floats[:2])
---
+++ list.py
@@ -0,0 +1,8 @@
+li=[1,2,3,4,5]
+print(7 in  li)
+print(2 in li)
+
+mi = [10, 4.97, True, "mountain", [9, 8, 7]]
+li = list("cheese")
+print("e" in li)
+print("a" not in li)
